81658b2a66633422c140ab376f274af4ce755570
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2021-07-21 14:38:51 +0200

    MAINT setting up pytest + fix version number

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
+from akasthesia import __version__
 
 long_description = "Long description of the awesome coevolution package"
 
 setup(
     name='akasthesia',
-    version='0.0.0.a.dev0',
+    version=__version__,
     description='Awesome coevolution stuff',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)

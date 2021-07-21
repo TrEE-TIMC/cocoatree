5016544f544b62e97b68dd5c2a47f211861cb1cc
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2021-07-21 20:53:23 +0200

    DOC S1A serine protease protein example
    - Removed some plots and information that are not necessary to the
    pipeline.
    - Better visualization of the ICs

--- examples/plot_sca.py
+++ examples/plot_sca.py
@@ -1,67 +1,100 @@
 """
-=====================================
-Running SCA on the S1A protein family
-=====================================
+=============================================
+Running SCA on the S1A serine protease family
+=============================================
 
 This example showcases how to run the whole SCA pipeline on the S1A protein
 family. This reproduces the results of Halabi et al, 2009.
 """
 
-from akasthesia.coevolution import Alignment, CoevolutionMatrix
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Reading from files
+from sklearn.preprocessing import quantile_transform
+from akasthesia.coevolution import Alignment, CoevolutionMatrix
+
+###############################################################################
+# ## Loading the S1A serine data
+#
+# This example is based on the serine protein family, studied by Halabi et al,
+# and then further by Rivoire at al.
 alg = Alignment.from_file("data/s1Ahalabi_1470.an")
-print("Loaded " + str(alg.seq_count()) + " sequence of size " + str(alg.seq_len()))
-print("After trimming:", str(np.shape(alg.filtered_alignment())[0]),
-      "sequences of length", str(np.shape(alg.filtered_alignment())[1]))
+
+n_sequences = alg.seq_count()
+n_residues = alg.seq_len()
+print("Loaded %d sequence of with %d residues" % (n_sequences, n_residues))
 
 ###############################################################################
-# First: Calculate Weights - Sequence Weight and Position Weights
+# ## Calculate Weights - Sequence Weight and Position Weights
+#
+# The first step of the pipeline consists in evaluating two sets of weights.
+# First, what we denote by sequence weight or organism weight: these weights
+# consists in correcting for the "phylogeny effect," ie the effect of an
+# uneven sampling of sequences due to the overreprentation of some organisms
+# in datasets (such as E. coli). The second set of weights corresponds to what
+# we denote by the "residue weights" or "position weights." These weights aim
+# at correcting for the fact that some residues are much more conserved than
+# others.
 
-sca_weights = alg.sca_seq_weights()
-pos_weights = alg.position_weights_sca(sca_weights)
+seq_weights = alg.sca_seq_weights()
+pos_weights = alg.position_weights_sca(seq_weights)
 
 ###############################################################################
-#  Coevolution matrix - weighted
+#  ## Estimating the coevolution matrix
+#
+# Then, we estimate the co-evolution matrix from the alignment, corrected with
+# these sequence and residue weights. The goal here is to estimate robustly
+# whether pairs of residues co-evolve.
 
-weighted_cij = alg.sca_coevolution(seq_weights=sca_weights,
+weighted_cij = alg.sca_coevolution(seq_weights=seq_weights,
                                    pos_weights=pos_weights)
 
 figure, ax = plt.subplots()
-figure.set_size_inches(20, 10)
 im = ax.matshow(weighted_cij.matrix)
 ax.set_yticks(range(0, np.shape(weighted_cij.matrix)[0], 40))
 ax.set_xticks(range(0, np.shape(weighted_cij.matrix)[0], 40))
 cbar = ax.figure.colorbar(im, ax=ax)
 
-plt.show()
+###############################################################################
+# ## Statistical analysis using Independant Component Analysis (ICA)
+#
+# The last step of the analysis consists in performing somewhat of an ICA on
+# the coevolution matrix estimated in the previous steps.
+
+n_components = 6
+vica, w = CoevolutionMatrix.ICA(weighted_cij.matrix, kmax=n_components)
+
+# Set some colors based on the projects on the second component
+colors = quantile_transform(
+    vica[:, 1].reshape(-1, 1), n_quantiles=n_residues).flatten()
+fig, axes = plt.subplots(n_components-1, n_components-1, tight_layout=True)
+for i, j in itertools.product(range(n_components), range(n_components)):
+    if i >= j:
+        if j >= 1 and (i < n_components-1):
+            fig.delaxes(axes[i, j-1])
+        continue
+    ax = axes[i, j-1]
+    ax.scatter(vica[:, j], vica[:, i], marker=".", c=colors)
+    ax.spines['left'].set_position(('data', 0))
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.spines["top"].set_linewidth(0)
+    ax.spines["right"].set_linewidth(0)
+
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    ax.tick_params(axis='both', which='major', labelsize="xx-small")
+    ax.tick_params(axis='both', which='minor', labelsize="xx-small")
+
+    ax.text(
+        xmin - (xmax - xmin) * 0.05, 0, "IC%d" % (i+1),
+        fontweight="bold", rotation=90, horizontalalignment="right",
+        fontsize="x-small", verticalalignment="center")
+    ax.text(
+        0, ymin - (ymax - ymin) * 0.05,
+        "IC%d" % (j+1), fontsize="x-small", fontweight="bold",
+        horizontalalignment="center", verticalalignment="top")
 
 ###############################################################################
-#  Perform ICA
-
-vica, w = CoevolutionMatrix.ICA(weighted_cij.matrix)
-
-figure, ax = plt.subplots(2, 3)
-figure.set_size_inches(15, 10)
-ax[0, 0].scatter(x=vica[:, 0], y=vica[:, 1])
-ax[0, 1].scatter(x=vica[:, 2], y=vica[:, 3])
-ax[0, 2].scatter(x=vica[:, 4], y=vica[:, 5])
-ax[1, 0].scatter(x=vica[:, 1], y=vica[:, 2])
-ax[1, 1].scatter(x=vica[:, 3], y=vica[:, 4])
-ax[1, 2].scatter(x=vica[:, 0], y=vica[:, 5])
-
-###############################################################################
-#  IC Histogram
-
-figure, axes = plt.subplots(2, 3)
-figure.set_size_inches(15, 10)
-
-i = 0
-for ax1 in axes:
-    ax1[0].set_ylabel("Count")
-    for ax in ax1:
-        ax.hist(vica[:, i], bins=20, rwidth=0.95)
-        i += 1
-        ax.set_xlabel("IC" + str(i))
+# We can see from the results of the ICA that some components clearly captures
+# clusters of residues. These likely correspond to sectors.

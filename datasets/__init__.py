a4bfd3c4ad216a3457fba1b832e43c8512f3f1a8
lamdv <lam.dv@live.com>
2021-09-01 17:12:20 +0200

    add datasets submodule; simulation code

---
+++ datasets/__init__.py
@@ -0,0 +1,68 @@
+from akasthesia.coevolution import Alignment
+import numpy as np
+
+import simulation
+
+
+def s1a(threshold=.2):
+    """S1A Serine Protease protein family
+
+    Parameters
+    ------
+    threshold : float
+        Default .2
+
+    Returns
+    ------
+    Alignment :
+        Alignment object of S1A protein family
+    """
+    return Alignment.from_file('data/s1Ahalabi_1470.an', threshold=threshold)
+
+
+def simple_simulated():
+    """Generate a simple simulated dataset
+
+    Returns:
+    --------
+    Alignment :
+        Alignment object of the generated dataset
+    """
+    N = 100
+    L = 75
+    init_seqs = np.random.randint(20, size=[N, L])
+    v, w = simulation.generate_v_and_w(L, 'simple', [])
+    seqs = simulation.gibbs_sampling(init_seqs, v, w, 100)
+    return simulation.to_Alignment(simulation.num_to_aa(seqs, N), N)
+
+
+def simulated(N, v, w, T):
+    """Generate simulated dataset using Gibbs sampling
+
+    Take the model in the form of v (Lx20) and w (LxLx20x20)
+    and generate a MSA data set. The probability of encountering 
+    a sequence is
+
+    .. math:
+        p(x|v,w) ~ exp(sum{i}(v_i(x_i)) + sum{ij}(w_ij(x_i, x_j)))
+
+
+    Parameters
+    -------
+    N : int
+        Number of sequences in the output MSA
+    v, w : np array
+        the model as descibed
+    t : int
+        the number of Gibbs steps
+
+    Returns
+    -------
+    Alignment :
+        Alignment object of the generated MSA
+
+    """
+    L = v.shape[0]
+    init_seqs = np.random.randint(20, size=[N, L])
+    seqs = simulation.gibbs_sampling(init_seqs, v, w, T)
+    return simulation.to_Alignment(simulation.num_to_aa(seqs, N), N)
---
+++ datasets/simulation.py
@@ -0,0 +1,150 @@
+# %%
+import numpy as np
+from akasthesia.coevolution import Alignment
+
+aa = '-ACDEFGHIKLMNPQRSTVWY'
+
+
+# %%
+# Gibbs sampling
+def conditional_prob(xt, res, v, w):
+    """Calculate conditional probability from v and w"""
+    j = np.delete(np.arange(len(xt)), res)
+    pot = np.exp(v[res] + np.sum(w[res, j, :, xt[j]], axis=0))
+    return (pot).T/np.sum(pot)
+
+
+def gibbs_sampling(init_seq, v, w, T):
+    """Gibbs sampling process
+
+    Return a simulated MSA in numerical represenations, according to
+    the model v and w using Gibbs sampling process
+
+    Parameters
+    ----------
+    init_seq : np array of initial sequences
+    v, w : np array
+        the statistical model
+    T : int
+        number of gibbs steps
+
+    Results
+    -------
+    np array (LxN)
+        Result of simulation
+    """
+    seqs = np.copy(init_seq)
+    [N, L] = seqs.shape
+    for _ in range(T):
+        for i in range(N):
+            for a in range(L):
+                cond_prob = conditional_prob(seqs[i], a, v, w)
+                seqs[i, a] = np.random.choice(20, p=cond_prob)
+    return seqs
+
+
+# %%
+# Support functions
+def num_to_aa(num_seqs, N):
+    """Return a amino acid represenation of the MSA from numerical"""
+    seqs = []
+    for i in range(N):
+        seqs.append([aa[_+1] for _ in num_seqs[:, i]])
+    return np.array(seqs)
+
+
+def to_Alignment(seqs, N):
+    """Generate a set of headers for Cocoa's Alignment objects
+
+    Parameters:
+        seqs : nparray (LxN)
+            amino acid representation of the MSA
+
+    Returns
+    -------
+    Alignment :
+        Alignment object of the MSA
+    """
+    headers = []
+    for i in range(N):
+        headers.append(" ".join(["Generated sequence No. ", str(i)]))
+    return Alignment(headers, seqs, 0)
+
+
+# %%
+# Generate parameters
+def generate_v(L, type_of_v, exclusion=[]):
+    # Define v
+    if type_of_v == 'simple':
+        v = np.ones([L, 20])
+    else:
+        choices = np.delete(np.arange(L), exclusion)
+        v_highrange = [8,  9]
+        v_lowrange = [-0.02,  0.05]
+        high_v_residue = np.random.choice(choices, size=20, replace=False)
+        low_v_residue = [i for i in range(L) if i not in high_v_residue]
+        v = np.ones([L, 20])
+        for i in high_v_residue:
+            v[i] = 0
+            for j in np.random.choice(range(0, 20), 3):
+                v[i, j] = np.random.uniform(v_highrange[0], v_highrange[1])
+        for i in low_v_residue:
+            v[i] = v[i]*np.random.uniform(v_lowrange[0], v_lowrange[1])
+    return v
+
+
+def generate_topology(nodes,  topo,  n_edges=None):
+    # edge nomenclature: [i, a, j, b]: amino acid a at res i to aa b at j
+    edges = []
+    r = len(nodes)
+    if topo == 'line':
+        for i in range(r-1):
+            if nodes[i][0] < nodes[i+1][0]:
+                edges.append(nodes[i]+nodes[i+1])
+
+    elif topo == 'circle':
+        for i in range(r-1):
+            edges.append(nodes[i]+nodes[i+1])
+        edges.append(nodes[-1]+nodes[0])
+
+    elif topo == 'fully-connected':
+        for i in range(r):
+            for j in range(i+1, r):
+                if nodes[i][0] != nodes[j][0]:
+                    edges.append(nodes[i]+nodes[j])
+
+    elif topo == 'randomly-connected':
+        # 50% of fully connected edges are removed
+        if n_edges is None:
+            print('number of edges is required for random connection')
+            return None
+        for i in range(r):
+            for j in range(i+1, r):
+                edges.append(nodes[i]+nodes[j])
+        edges = [edges[retain] for retain in
+                 [i for i in np.random.default_rng().choice(range(len(edges)),
+                  size=n_edges, replace=False)]]
+    return edges
+
+
+def generate_w(L, edges):
+    # edge nomenclature: [i, a, j, b]: amino acid a at res i to aa b at j
+    w = np.ones([L, L, 20, 20])*-1
+    for edge in edges:
+        if edge[0] == edge[2]:
+            continue
+        w[edge[0], edge[2], edge[1], edge[3]] = 5  # a@i to b@j
+        w[edge[2], edge[0], edge[3], edge[1]] = 5  # b@j to a@b
+    return w
+
+
+def generate_v_and_w(L, v_type, edges):
+    # Create custom binary matrices
+    exclusion = []
+    for edge in edges:
+        exclusion += [edge[0], edge[2]]
+    x_single = generate_v(L, v_type, exclusion)
+    x_pair = generate_w(L, edges)
+    return x_single, x_pair
+
+# %%

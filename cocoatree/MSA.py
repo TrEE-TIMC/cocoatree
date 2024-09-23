148f470504774f3ff79e34afa4fac61c87611583
margauxjullien <margauxjullien3@gmail.com>
2024-09-23 15:18:17 +0200

    add alignment filtering functions

--- cocoatree/MSA.py
+++ cocoatree/MSA.py
@@ -72,3 +72,129 @@ def _clean_msa(msa):
                 msa[index].seq = Seq(sequence)
 
     return msa
+
+
+def filter_gap_pos(sequences, threshold=0.4):
+    """Filter the sequences for overly gapped positions.
+
+    Arguments
+    ---------
+    sequences : list of the MSA sequences to filter
+
+    threshold : max proportion of gaps tolerated (default=0.4)
+
+    Returns
+    -------
+    filt_seqs : list of the sequences after filter
+
+    pos_kept : numpy.ndarray of the positions that were conserved
+    """
+
+    print("Filter MSA for overly gapped positions")
+
+    Nseq, Npos = len(sequences), len(sequences[0])
+
+    gaps = np.array([[int(sequences[seq][pos] == '-') for pos in range(Npos)] for seq in range(Nseq)])
+
+    freq_gap_per_pos = np.cumsum(gaps, axis=0)[-1] / Nseq
+
+    pos_kept = np.where(freq_gap_per_pos <= threshold)[0]
+
+    print("Keeping %i out of %i positions" % (len(pos_kept), Npos))
+
+    filt_seqs = ["".join([sequences[seq][pos] for pos in pos_kept]) for seq in range(Nseq)]
+
+    return filt_seqs, pos_kept
+
+
+def filter_gap_seq(seq_id, sequences, threshold=0.2, filtrefseq=False,
+                   refseq_id=None):
+    """
+    Remove sequences with a fraction of gaps greater than a specified
+    value.
+    Also possibility to remove sequences with sequence identity too high
+    with a given reference sequence.
+
+    Arguments
+    ---------
+    seq_id : list of the MSA's sequence identifiers
+
+    sequences : list of MSA sequences
+
+    threshold : maximum fraction of gaps per sequence (default 0.2)
+
+    filtrefseq : boolean, whether to filter based on a reference sequence
+                 (default False)
+
+    refseq_id : str, default = None
+                identifier of the reference sequence (only if filtrefseq=True)
+                If 'None', a default reference sequence is chosen
+
+    Returns
+    -------
+    seq_id_kept : list of conserved sequence identifiers
+
+    seq_kept : list of kept sequences
+    """
+
+    print('Filter MSA for overly gapped sequences')
+
+    Nseq, Npos = len(sequences), len(sequences[0])
+
+    freq_gap_per_seq = np.array([sequences[seq].count('-') / Npos for seq in range(Nseq)])
+
+    seq_kept_index = np.where(freq_gap_per_seq <= threshold)[0]
+    print('Keeping %i sequences out of %i sequences' %
+          (len(seq_kept_index), Nseq))
+
+    seq_kept = [sequences[seq] for seq in seq_kept_index]
+    seq_id_kept = [seq_id[seq] for seq in seq_kept_index]
+
+    if filtrefseq:
+        print('Remove sequences too similar to the reference sequence')
+        seq_id_kept, seq_kept = filter_ref_seq(seq_id_kept, seq_kept,
+                                               delta=0.2, refseq_id=refseq_id)
+
+    return seq_id_kept, seq_kept
+
+
+def filter_ref_seq(seq_id, sequences, delta=0.2, refseq_id=None):
+    '''
+    Remove sequences r with Sr < delta, where Sr is the fractional identity
+    between r and a specified reference sequence.
+
+    Arguments
+    ---------
+    seq_id : list of sequence identifiers in the MSA
+
+    sequences : list of sequences in the MSA
+
+    delta : identity threshold (default 0.2)
+
+    refseq_id : identifier of the reference sequence, if 'None', a reference
+                sequence is computed (default 'None')
+
+    Returns
+    -------
+    seq_id_kept : list of identifiers of the kept sequences
+
+    seq_kept : list of the kept sequences
+    '''
+
+    Nseq = len(sequences)
+
+    if refseq_id is None:
+        print('Choose a default reference sequence within the alignment')
+        refseq_idx = refSeq(sequences)
+    else:
+        print('Reference sequence is: %i' % refseq_id)
+        refseq_idx = seq_id.index(refseq_id)
+
+    sim_matrix = seq_identity(sequences, graphic=False)
+    seq_kept_index = np.where(sim_matrix[refseq_idx] >= delta)[0]
+    seq_kept = [sequences[seq] for seq in seq_kept_index]
+    seq_id_kept = [seq_id[seq] for seq in seq_kept_index]
+
+    print('Keeping %i out of %i sequences' % (len(seq_kept), Nseq))
+
+    return seq_id_kept, seq_kept

e03fc6e5eeefd8fc79dd22ffc4c38641f09cc830
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2024-10-01 16:07:04 +0200

    Adding some asserts in the tests

--- cocoatree/datasets/tests/test_datasets.py
+++ cocoatree/datasets/tests/test_datasets.py
@@ -3,3 +3,5 @@ from cocoatree.datasets import load_S1A_serine_proteases
 
 def test_load_S1A_serine_proteases():
     dataset = load_S1A_serine_proteases()
+    assert "sequence_ids" in dataset.keys()
+    assert "alignment" in dataset.keys()

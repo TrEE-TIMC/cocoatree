a34d68d853f673778a1ddb1006a588db3c08f70f
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2025-01-15 16:09:00 +0100

    Clean up on randomize

--- cocoatree/randomize.py
+++ cocoatree/randomize.py
@@ -8,16 +8,16 @@ from .statistics.pairwise import aa_joint_freq, compute_sca_matrix
 from .deconvolution import eigen_decomp
 
 
-def _random_aln(fia, Nseq):
+def _random_aln(fia, n_seq):
     """
-    Generate a random alignment with Nseq sequences and amino acid frequency
+    Generate a random alignment with n_seq sequences and amino acid frequency
     at each position fia
 
     Arguments
     ---------
     fia : frequency of amino acid *a* at position *i*
 
-    Nseq : number of sequences
+    n_seq : number of sequences
 
     Returns
     -------
@@ -26,10 +26,10 @@ def _random_aln(fia, Nseq):
     binarray : Random alignment as a binary array
     """
 
-    Npos = fia.shape[0]
-    msa_rand = np.zeros((Nseq, Npos), dtype=int)
-    for i in range(Npos):
-        Maa = np.random.multinomial(Nseq, fia[i, :])
+    n_pos = fia.shape[0]
+    msa_rand = np.zeros((n_seq, n_pos), dtype=int)
+    for i in range(n_pos):
+        Maa = np.random.multinomial(n_seq, fia[i, :])
         col = np.array([], dtype=int)
         for aa, M in enumerate(Maa):
             col = np.append(col, np.tile(aa, M))
@@ -51,8 +51,7 @@ def _random_aln(fia, Nseq):
     return msa_str, binarray
 
 
-def randomization(sequences, Nrep, weights=1, lambda_coef=0.03, kmax=6,
-                  metric='SCA', correction=None):
+def randomization(sequences, n_rep, weights=1, lambda_coef=0.03, kmax=6):
     """
     Randomize the alignment while preserving the frequencies of amino acids at
     each position and compute the resulting spectrum of coevolution matrix.
@@ -61,20 +60,14 @@ def randomization(sequences, Nrep, weights=1, lambda_coef=0.03, kmax=6,
     ---------
     sequences : multiple sequence alignment
 
-    Nrep : number of iterations of randomization
-
-    Naa : number of amino acids to consider, default = 21
+    n_rep : number of iterations of randomization
 
     weights : vector of sequence weights (default = assume equal weights)
 
-    lbda : pseudo-counts frequency
+    lambda_coef : float, optional, default: 0.03
+        pseudo-counts
 
     kmax : number of eigenvectors to keep for each randomized iteration
-
-    metric : coevolution metric to use, 'MI' or 'SCA' (default = 'SCA')
-
-    correction : correction to apply on wthe coevolution matrix, 'APC',
-            'entropy' or None (default = None)
 
     Returns
     -------
@@ -83,35 +76,27 @@ def randomization(sequences, Nrep, weights=1, lambda_coef=0.03, kmax=6,
     val_rand :
     """
 
-    Nseq, Npos = len(sequences), len(sequences[0])
+    n_seq, n_pos = len(sequences), len(sequences[0])
 
     # Create a vector of sequence weights = 1 if equal weighting
     if isinstance(weights, int) and weights == 1:
-        weights = np.ones(Nseq)
+        weights = np.ones(n_seq)
 
     fia = aa_freq_at_pos(sequences, lambda_coef=lambda_coef, weights=weights)
     background_freq = compute_background_frequencies(fia,
                                                      lambda_coef=lambda_coef)
 
     # initialize for eigenvectors
-    vect_rand = np.zeros((Nrep, Npos, kmax))
+    vect_rand = np.zeros((n_rep, n_pos, kmax))
     # initialize for eigenvalues
-    val_rand = np.zeros((Nrep, Npos))
-    for rep in range(Nrep):
-        msa_random = _random_aln(fia, Nseq)[0]
+    val_rand = np.zeros((n_rep, n_pos))
+    for rep in range(n_rep):
+        msa_random = _random_aln(fia, n_seq)[0]
         fijab, fijab_ind = aa_joint_freq(msa_random, weights,
                                          lambda_coef=lambda_coef)
         # Compute coevolution matrix for the randomized alignment
-        if metric == 'SCA':
-            Coev_rand = compute_sca_matrix(fijab, fijab_ind, fia,
-                                           background_freq)[1]
-        # elif metric == 'MI':
-        #    Coev_rand = compute_mi_matrix(fijab, fijab_ind)
-        # add a correction
-        # if correction == 'APC':
-        #    Coev_rand = APC(Coev_rand)
-        # if correction == 'entropy':
-        #    Coev_rand = entropy_correction(Coev_rand)
+        Coev_rand = compute_sca_matrix(fijab, fijab_ind, fia,
+                                       background_freq)[1]
 
         eig_val, eig_vec = eigen_decomp(Coev_rand)
         vect_rand[rep, :, :] = eig_vec[:, :kmax]

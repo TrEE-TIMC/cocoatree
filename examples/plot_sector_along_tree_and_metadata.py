7859fdbb2ffeed5978a2a991eb549aadf2b06bab
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2025-01-16 09:04:03 +0100

    FIX pep8 issues in example

--- examples/plot_sector_along_tree_and_metadata.py
+++ examples/plot_sector_along_tree_and_metadata.py
@@ -21,10 +21,9 @@ tree_ete3 = load_tree_ete3(tree_file)
 sector_file = 'data/SCA_sector_1_core_tagged_seq.fasta'
 sector_id, sector_seq = load_MSA(sector_file, 'fasta')
 
-tree_style = update_tree_ete3_and_return_style(    
+tree_style = update_tree_ete3_and_return_style(
     tree_ete3, df_annot, sector_id, sector_seq,
-    #meta_data=('HMM_annotation',),
-    meta_data=('class','HMM_annotation','superkingdom'),
+    meta_data=('class', 'HMM_annotation', 'superkingdom'),
     fig_title='Test HMM annot',
     t_sector_seq=False,
     t_sector_heatmap=True,

78d90fe1c37b825120e5a47342050ed7efc4dd0e
Ivan @ laptop <ivan.junier@univ-grenoble-alpes.fr>
2025-01-31 10:28:09 +0100

    debugged examples using the new code

--- examples/plot_sca_vs_mi.py
+++ examples/plot_sca_vs_mi.py
@@ -10,12 +10,13 @@ serine proteases using SCA and the mutual information.
 from cocoatree.datasets import load_S1A_serine_proteases
 from cocoatree.msa import filter_sequences
 
-from cocoatree.statistics import compute_all_frequencies
+from cocoatree.statistics.position import compute_conservation
 from cocoatree.statistics.pairwise import compute_sca_matrix
 from cocoatree.statistics.pairwise import compute_mutual_information_matrix
 
 import matplotlib.pyplot as plt
 
+import np as np
 
 # %%
 # Load the dataset
@@ -38,32 +39,80 @@ seq_id_kept, seq_kept, pos_kept = filter_sequences(seq_id, sequences)
 # %%
 # Compute the SCA matrix
 # ----------------------
-
-aa_freq, bkgd_freqs, aa_joint_freqs = compute_all_frequencies(
-    seq_kept, seq_weights=None)
-sca = compute_sca_matrix(aa_joint_freqs=aa_joint_freqs,
-                         aa_freqs=aa_freq,
-                         bkgd_freqs=bkgd_freqs)
+SCA_matrix = compute_sca_matrix(seq_kept)
 
 # %%
 # Compute the Mutual information matrix
 # -------------------------------------
-normalized_mi = compute_mutual_information_matrix(seq_kept)
-mi = compute_mutual_information_matrix(seq_kept, normalize=False)
+normalized_MI = compute_mutual_information_matrix(seq_kept)
+MI = compute_mutual_information_matrix(seq_kept, normalize=False)
 
 # %%
 # Compare MI versus SCA
+# ---------------------
+plt.figure(figsize=(12, 5))
 
+for ih, (heatmap, title) in \
+    enumerate(zip([SCA_matrix, MI, normalized_MI],
+                  ['SCA matrix', 'Mutual Information',
+                   'Normalized Mutual Information'])):
+    plt.subplot(1, 3, ih+1)
+    plt.imshow(heatmap, cmap='inferno')
+    plt.xlabel('residues', fontsize=10)
+    plt.ylabel(None)
+    plt.title('%s' % title)
+    plt.colorbar(shrink=0.4)
 
-fig, axes = plt.subplots(ncols=3, figsize=(8, 4), tight_layout=True)
-ax = axes[0]
-ax.matshow(sca)
-ax.set_title("SCA matrix")
+# %%
+# Adapt heatmap scale
+# ---------------------
+plt.figure(figsize=(12, 5))
 
-ax = axes[1]
-ax.matshow(normalized_mi)
-ax.set_title("Normalized Mutual Information")
+for ih, (heatmap, title, vmm) in \
+    enumerate(zip([SCA_matrix, MI, normalized_MI],
+                  ['SCA matrix', 'Mutual Information',
+                   'Normalized Mutual Information'],
+                  [[0, 1], [0, 0.8], [0, 0.2]])):
+    plt.subplot(1, 3, ih+1)
+    plt.imshow(heatmap, vmin=vmm[0], vmax=vmm[1], cmap='inferno')
+    plt.xlabel('residues', fontsize=10)
+    plt.ylabel(None)
+    plt.title('%s' % title)
+    plt.colorbar(shrink=0.4)
 
-ax = axes[2]
-ax.matshow(mi)
-ax.set_title("Mutual Information")
+# %%
+# Relationships between SCA and MI's
+# ----------------------------------
+plt.figure(figsize=(12, 3))
+plt.subplot(1, 3, 1)
+plt.plot(np.triu(SCA_matrix, 1).flatten(),
+         np.triu(normalized_MI, 1).flatten(), 'o')
+plt.xlabel('SCA matrix')
+plt.ylabel('Normalized Mutual Information')
+
+plt.subplot(1, 3, 2)
+plt.plot(np.triu(MI, 1).flatten(), np.triu(normalized_MI, 1).flatten(), 'o')
+plt.xlabel('Mutual Information')
+plt.ylabel('Normalized Mutual Information')
+
+plt.subplot(1, 3, 3)
+plt.plot(np.triu(SCA_matrix, 1).flatten(), np.triu(MI, 1).flatten(), 'o')
+plt.xlabel('SCA matrix')
+plt.ylabel('Mutual Information')
+
+# %%
+# Relationship between coevolution metrics and conservation
+# ---------------------------------------------------------
+plt.figure(figsize=(12, 3))
+
+Di = compute_conservation(seq_kept)
+
+for ih, (heatmap, title) in enumerate(zip([SCA_matrix, MI, normalized_MI],
+                                          ['SCA', 'MI', 'normalized MI'])):
+    plt.subplot(1, 3, ih + 1)
+    cumul = np.sum(heatmap, axis=0) - np.diag(heatmap)
+    plt.plot(Di, cumul, 'o')
+    plt.xlabel('conservation')
+    if ih == 0:
+        plt.ylabel('cumulative score')
+    plt.title(title)

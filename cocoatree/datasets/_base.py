f072df5de3360e7d20981af835883ac25cfcd3d9
margauxjullien <margauxjullien3@gmail.com>
2025-02-11 18:03:23 +0100

    update S1A_serine_protease dataset

--- cocoatree/datasets/_base.py
+++ cocoatree/datasets/_base.py
@@ -1,14 +1,22 @@
 import os
 from ..io import load_MSA
+import numpy as np
+import pandas as pd
 
 
-def load_S1A_serine_proteases():
+def load_S1A_serine_proteases(paper):
     """
     Load the S1A serine protease dataset
 
     Note that this is not the original dataset from Halabi et al,
     Cell, 2008. The dataset has been augmented with more recent data
     (hence the MSA is larger), and reprocessed.
+
+    Parameters
+    ----------
+    paper: str, either 'halabi' or 'rivoire'
+        whether to load the dataset from Halabi et al, Cell, 2008 or from
+        Rivoire et al, PLoS Comput Biol, 2016
 
     Returns
     -------
@@ -17,16 +25,50 @@ def load_S1A_serine_proteases():
         - `sequences_ids`: a list of strings corresponding to sequence names
         - `alignment`: a list of strings corresponding to sequences. Because it
           is an MSA, all the strings are of same length.
+        - `metadata`: a pandas dataframe containing the metadata associated
+          with the alignment.
+        - `sector_positions`: a dictionnary of arrays containing the residue
+          positions associated to sectors, either in Halabi et al, or in
+          Rivoire et al.
     """
 
     module_path = os.path.dirname(__file__)
-    filename = os.path.join(
-        module_path,
-        "data/S1A_serine_proteases/alignment.fasta")
-    sequence_ids, sequences = load_MSA(filename, format="fasta")
+
+    if paper == 'halabi':
+        # Load the alignment used in Halabi et al, 2008
+        filename = os.path.join(
+            module_path,
+            "data/S1A_serine_proteases/halabi_alignment.fasta")
+        sequence_ids, sequences = load_MSA(filename, format="fasta")
+        # Load the positions of the 3 sectors identified in Halabi et al, Cell,
+        # 2008
+        filename = os.path.join(
+            module_path,
+            "data/S1A_serine_proteases/halabi_sectors.npz")
+        sectors = np.load(filename)
+
+    if paper == 'rivoire':
+        # Load the alignment used in Rivoire et al, 2016
+        filename = os.path.join(
+            module_path,
+            "data/S1A_serine_proteases/rivoire_alignment.fasta")
+        sequence_ids, sequences = load_MSA(filename, format="fasta")
+        # Load the positions of the 6 sectors identified in Rivoire et al, PLoS
+        # Comput Biol, 2016
+        filename = os.path.join(
+            module_path,
+            "data/S1A_serine_proteases/rivoire_sectors.npz")
+        sectors = np.load(filename)
+        # Load the metadata
+        filename = os.path.join(
+            module_path,
+            "data/S1A_serine_proteases/rivoire_metadata.csv")
+        metadata = pd.read_csv(filename)
 
     return {"sequence_ids": sequence_ids,
-            "alignment": sequences}
+            "alignment": sequences,
+            "sector_positions": sectors,
+            "metadata": metadata}
 
 
 def load_rhomboid_proteases():

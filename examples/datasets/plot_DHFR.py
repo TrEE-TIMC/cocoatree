18814d971329b9f1f123cfe1a621312c170c25cf
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2025-07-01 11:54:28 +0200

    DOC output statistics for all of the datasets

--- examples/datasets/plot_DHFR.py
+++ examples/datasets/plot_DHFR.py
@@ -5,7 +5,29 @@ DHFR proteases
 Load the dataset
 
 """
+import numpy as np
 
 from cocoatree.datasets import load_DHFR
+import cocoatree.msa as c_msa
+import cocoatree.statistics.position as c_pos
+
 
 dataset = load_DHFR()
+
+print("Number of sequences", len(dataset["alignment"]))
+loaded_seqs = dataset["alignment"]
+loaded_seqs_id = dataset["sequence_ids"]
+n_loaded_pos, n_loaded_seqs = len(loaded_seqs[0]), len(loaded_seqs)
+
+print(f"The loaded MSA has {n_loaded_seqs} sequences and {n_loaded_pos} \
+      positions.")
+
+sequences, sequences_id, positions = c_msa.filter_sequences(
+    loaded_seqs, loaded_seqs_id, gap_threshold=0.4, seq_threshold=0.2)
+n_pos = len(positions)
+print(f"After filtering, we have {n_pos} remaining positions.")
+print(f"After filtering, we have {len(sequences)} remaining sequences.")
+
+seq_weights, m_eff = c_pos.compute_seq_weights(sequences)
+print('Number of effective sequences %d' %
+      np.round(m_eff))

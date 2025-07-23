6f6a8b88344b5f233a231c70d7130f0779fd57c8
M.Chauveau <chauveaumarion4@gmail.com>
2025-07-23 21:48:49 +0200

    add space before comment to ignore flake8 error

--- cocoatree/__init__.py
+++ cocoatree/__init__.py
@@ -1,9 +1,9 @@
-from . import msa # noqa: F401
-from . import datasets # noqa: F401
-from . import statistics # noqa: F401
-from . import io # noqa: F401
-from . import deconvolution # noqa: F401
-from ._pipeline import perform_sca # noqa: F401
+from . import msa  # noqa: F401
+from . import datasets  # noqa: F401
+from . import statistics  # noqa: F401
+from . import io  # noqa: F401
+from . import deconvolution  # noqa: F401
+from ._pipeline import perform_sca  # noqa: F401
 
 
 __version__ = "0.0.0a0.dev0"

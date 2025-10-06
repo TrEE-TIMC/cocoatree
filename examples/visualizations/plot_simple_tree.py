018cb242b3e7880485bbf35fc9c508f455d50f3b
margauxjullien <margauxjullien3@gmail.com>
2025-10-06 09:15:29 +0200

    change dataset used in viz examples

--- examples/visualizations/plot_simple_tree.py
+++ examples/visualizations/plot_simple_tree.py
@@ -9,16 +9,17 @@ A simple tree visualization with cocoatree.
 
 import pandas as pd
 from cocoatree.io import load_MSA, load_tree_ete3
+from cocoatree.datasets import load_S1A_serine_proteases
 from cocoatree.visualization import update_tree_ete3_and_return_style
 
 
-annot_file = 'data/random_seq_tree_annotations.csv'
-df_annot = pd.read_csv(annot_file)
+serine_dataset = load_S1A_serine_proteases('halabi')
+df_annot = serine_dataset["metadata"]
 
-tree_file = 'data/random_seq_tree_kpicsg_ete3.treefile'
+tree_file = 'data/halabi_82_seqs.txt'
 tree_ete3 = load_tree_ete3(tree_file)
 
-sector_file = 'data/SCA_sector_1_core_tagged_seq.fasta'
+sector_file = 'data/halabi_sector_1_SCA.fasta'
 data = load_MSA(sector_file, 'fasta')
 
 sector_id = data["sequence_ids"]
@@ -26,7 +27,7 @@ sector_seq = data["alignment"]
 
 tree_style, column_layout = update_tree_ete3_and_return_style(
     tree_ete3, df_annot,
-    meta_data=("superkingdom", 'class', 'HMM_annotation'),
+    meta_data=('Protein_type', 'Class', 'Family'),
     fig_title='A tree visualization with metadata',
     )
 
--- examples/visualizations/plot_tree_metadata_colors.py
+++ examples/visualizations/plot_tree_metadata_colors.py
@@ -9,45 +9,55 @@ The user can provide a dictionnary of colors for metadata.
 
 import pandas as pd
 from cocoatree.io import load_MSA, load_tree_ete3
+from cocoatree.datasets import load_S1A_serine_proteases
 from cocoatree.visualization import update_tree_ete3_and_return_style
 
 
-annot_file = 'data/random_seq_tree_annotations.csv'
-df_annot = pd.read_csv(annot_file)
+serine_dataset = load_S1A_serine_proteases('halabi')
+df_annot = serine_dataset["metadata"]
 
-tree_file = 'data/random_seq_tree_kpicsg_ete3.treefile'
+tree_file = 'data/halabi_82_seqs.txt'
 tree_ete3 = load_tree_ete3(tree_file)
 
-sector_file = 'data/SCA_sector_1_core_tagged_seq.fasta'
+sector_file = 'data/halabi_sector_1_SCA.fasta'
 data = load_MSA(sector_file, 'fasta')
 
 sector_id = data["sequence_ids"]
 sector_seq = data["alignment"]
 
 
-colors = {
-    "Bacteria": "black",
-    "Eukaryota": "#cccccc",
-    "FMO_Cyano": "#fff5f0",
-    "UbiL": "#fdd4c2",
-    "UbiI": "#fca082",
-    "UbiH": "#fb694a",
-    "UbiF": "#e32f27",
-    "UbiN": "#b11218",
-    "UbiFHILM_Cyano": "#67000d",
-    }
+halabi_cmap = {
+    'vertebrate': '#798e87',
+    'invertebrate': '#c27d38',
+    'fungi': '#ccc591',
+    'bacteria': '#29211f',
+    'Chymotrypsin': '#00a08a',
+    'Trypsin': '#ff0000',
+    'Tryptase': '#f2ad00',
+    'Kallikrein': '#f98400',
+    'Granzyme': '#5bbcd6',
+    'Mammalia': '#C969A1',
+    'Actinopterygii': '#CE4441',
+    'Amphibia': '#EE8577',
+    'Malacostraca': '#EB7926',
+    'other': 'lightgrey',
+    'Insecta': '#FFBB44',
+    'Actinobacteria': '#859B6C',
+    'Arachnida': '#62929A',
+    'Oligochaeta': '#004F63'
+}
 
 
 tree_style, column_layout = update_tree_ete3_and_return_style(
     tree_ete3, df_annot, sector_id, sector_seq,
-    meta_data=("superkingdom", "HMM_annotation"),
-    fig_title='Visualization example',
-    metadata_colors=colors,
+    meta_data=('Protein_type', 'Class', 'Family'),
+    fig_title='Personnalized colormap example',
+    metadata_colors=halabi_cmap,
     linewidth=3,
     show_leaf_name=False,
     t_sector_seq=True,
     t_sector_heatmap=False,
-    colormap='inferno'
+    colormap='GnBu'
     )
 
 

5fb4ec202ddcfb2db0b85940d47730505f45afd0
margauxjullien <margauxjullien3@gmail.com>
2025-10-08 11:05:41 +0200

    fix flake8 issue

--- cocoatree/visualization.py
+++ cocoatree/visualization.py
@@ -423,7 +423,7 @@ def add_heatmap_to_tree(tree_style, tree_ete3, sector_id, sector_seq,
         matrix = compute_normalized_seq_similarity(reordered_sequences)
     # FIX to zero values appearing black in the heatmap whatever the cmap
     matrix[matrix == 0] = 0.00000001
-    
+
     # Add heatmap profile to each leaf
     for i, lf in enumerate(tree_ete3.iter_leaves()):
         lf.add_features(profile=matrix[i])

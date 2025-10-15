a5d2698f8b99880acc8f8000a4708451aa7ff65b
margauxjullien <margauxjullien3@gmail.com>
2025-10-15 14:57:24 +0200

    add references at the end of example

--- examples/d_datasets/plot_s1A_serine_proteases.py
+++ examples/d_datasets/plot_s1A_serine_proteases.py
@@ -2,15 +2,31 @@
 S1A serine proteases
 ====================
 
-Load the dataset
+.. image:: images/Chymotrypsin_enzyme.png
+    :width: 600px
+    :height: 600px
+    :alt: 3D structure of bovine chymotrypsin
+    :align: center
+
+This example shows how the S1A serine proteases dataset is organized and how you
+can use it to test cocoatree's features.
+
+This dataset is actually composed of two MSAs:
+    - the MSA from Halabi et al., (2008)
+    - the MSA from Rivoire et al., (2016)
 
 """
 
+# %% 
+# Imports
 import numpy as np
 from cocoatree.datasets import load_S1A_serine_proteases
 
 import cocoatree.msa as c_msa
 import cocoatree.statistics.position as c_pos
+
+# %%
+# :ref:`sphx_glr_auto_examples_plot_sector_along_tree_and_metadata.py`
 
 
 for paper in ["halabi", "rivoire"]:
@@ -35,3 +51,15 @@ for paper in ["halabi", "rivoire"]:
     print('Number of effective sequences %d' %
           np.round(m_eff))
     print()
+
+# %%
+# References
+# ----------
+#
+# .. [1] `Protein Sectors: Evolutionary Units of Three-Dimensional Structure
+#        <https://doi-org.insb.bib.cnrs.fr/10.1016/j.cell.2009.07.038>`_,
+#        Halabi et al., Cell 2008
+# .. [2] `Evolution-Based Functional Decomposition of Proteins
+#        <https://doi.org/10.1371/journal.pcbi.1004817>`_,
+#        Rivoire et al., PLOS Computational Biology 2016
+# 

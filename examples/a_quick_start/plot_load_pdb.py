133e7067f472e9c94381995543db3eba98eca1cb
margauxjullien <margauxjullien3@gmail.com>
2025-10-30 09:13:30 +0100

    fix flake8

--- examples/a_quick_start/plot_load_pdb.py
+++ examples/a_quick_start/plot_load_pdb.py
@@ -35,26 +35,26 @@ pdb_seq, pdb_pos = load_pdb('data/3TGI.pdb', pdb_id='3TGI', chain='E')
 # In order to understand the function's ``chain`` argument, you need to open
 # the PDB in an editor to check the information included within your file:
 
-"""HEADER    COMPLEX (SERINE PROTEASE/INHIBITOR)     15-JUL-98   3TGI              
-TITLE     WILD-TYPE RAT ANIONIC TRYPSIN COMPLEXED WITH BOVINE                   
-TITLE    2 PANCREATIC TRYPSIN INHIBITOR (BPTI)                                  
-COMPND    MOL_ID: 1;                                                            
-COMPND   2 MOLECULE: TRYPSIN;                                                   
-COMPND   3 CHAIN: E;                                                            
-COMPND   4 EC: 3.4.21.4;                                                        
-COMPND   5 MOL_ID: 2;                                                           
-COMPND   6 MOLECULE: BOVINE PANCREATIC TRYPSIN INHIBITOR;                       
-COMPND   7 CHAIN: I;                                                            
-COMPND   8 SYNONYM: BPTI                                                        
-SOURCE    MOL_ID: 1;                                                            
-SOURCE   2 ORGANISM_SCIENTIFIC: RATTUS NORVEGICUS;                              
-SOURCE   3 ORGANISM_COMMON: NORWAY RAT;                                         
-SOURCE   4 ORGANISM_TAXID: 10116;                                               
-SOURCE   5 ORGAN: PANCREATIC;                                                   
-SOURCE   6 MOL_ID: 2;                                                           
-SOURCE   7 ORGANISM_SCIENTIFIC: BOS TAURUS;                                     
-SOURCE   8 ORGANISM_COMMON: CATTLE;                                             
-SOURCE   9 ORGANISM_TAXID: 9913                          """
+"""HEADER    COMPLEX (SERINE PROTEASE/INHIBITOR)     15-JUL-98   3TGI
+TITLE     WILD-TYPE RAT ANIONIC TRYPSIN COMPLEXED WITH BOVINE
+TITLE    2 PANCREATIC TRYPSIN INHIBITOR (BPTI)
+COMPND    MOL_ID: 1;
+COMPND   2 MOLECULE: TRYPSIN;
+COMPND   3 CHAIN: E;
+COMPND   4 EC: 3.4.21.4;
+COMPND   5 MOL_ID: 2;
+COMPND   6 MOLECULE: BOVINE PANCREATIC TRYPSIN INHIBITOR;
+COMPND   7 CHAIN: I;
+COMPND   8 SYNONYM: BPTI
+SOURCE    MOL_ID: 1;
+SOURCE   2 ORGANISM_SCIENTIFIC: RATTUS NORVEGICUS;
+SOURCE   3 ORGANISM_COMMON: NORWAY RAT;
+SOURCE   4 ORGANISM_TAXID: 10116;
+SOURCE   5 ORGAN: PANCREATIC;
+SOURCE   6 MOL_ID: 2;
+SOURCE   7 ORGANISM_SCIENTIFIC: BOS TAURUS;
+SOURCE   8 ORGANISM_COMMON: CATTLE;
+SOURCE   9 ORGANISM_TAXID: 9913"""
 
 # %%
 # As you can see, there are actually two molecules in the PDB:
@@ -69,27 +69,32 @@ SOURCE   9 ORGANISM_TAXID: 9913                          """
 # reductase's PDB file (which is the one included in cocoatree's
 # :func:`cocoatree.datasets.load_DHFR`):
 
-"""HEADER    OXIDOREDUCTASE                          02-FEB-11   3QL0              
-TITLE     CRYSTAL STRUCTURE OF N23PP/S148A MUTANT OF E. COLI DIHYDROFOLATE      
-TITLE    2 REDUCTASE                                                            
-COMPND    MOL_ID: 1;                                                            
-COMPND   2 MOLECULE: DIHYDROFOLATE REDUCTASE;                                   
-COMPND   3 CHAIN: A;                                                            
-COMPND   4 EC: 1.5.1.3;                                                         
-COMPND   5 ENGINEERED: YES;                                                     
-COMPND   6 MUTATION: YES                                                        
-SOURCE    MOL_ID: 1;                                                            
-SOURCE   2 ORGANISM_SCIENTIFIC: ESCHERICHIA COLI;                               
-SOURCE   3 ORGANISM_TAXID: 364106;                                              
-SOURCE   4 STRAIN: UTI89 / UPEC;                                                
-SOURCE   5 GENE: FOLA, UTI89_C0054;                                             
-SOURCE   6 EXPRESSION_SYSTEM: ESCHERICHIA COLI;                                 
-SOURCE   7 EXPRESSION_SYSTEM_TAXID: 562                                    """
+"""HEADER    OXIDOREDUCTASE                          02-FEB-11   3QL0
+TITLE     CRYSTAL STRUCTURE OF N23PP/S148A MUTANT OF E. COLI DIHYDROFOLATE
+TITLE    2 REDUCTASE
+COMPND    MOL_ID: 1;
+COMPND   2 MOLECULE: DIHYDROFOLATE REDUCTASE;
+COMPND   3 CHAIN: A;
+COMPND   4 EC: 1.5.1.3;
+COMPND   5 ENGINEERED: YES;
+COMPND   6 MUTATION: YES
+SOURCE    MOL_ID: 1;
+SOURCE   2 ORGANISM_SCIENTIFIC: ESCHERICHIA COLI;
+SOURCE   3 ORGANISM_TAXID: 364106;
+SOURCE   4 STRAIN: UTI89 / UPEC;
+SOURCE   5 GENE: FOLA, UTI89_C0054;
+SOURCE   6 EXPRESSION_SYSTEM: ESCHERICHIA COLI;
+SOURCE   7 EXPRESSION_SYSTEM_TAXID: 562"""
 
 # %%
-# In this case, there is only one molecule, which is specified by
+# In this case, there is only one molecule, which is accessed by specifying
 # ``chain='A'``.
 #
 # You can now access the pdb sequence and the residue numbering:
 print(pdb_seq)
 print(pdb_pos)
+
+# ..seealso::
+#       :ref:`sphx_glr_auto_examples_a_quick_start_plot_map_alignments.py` to
+#       see how to perform a mapping of the positions between your MSA and the
+#       PDB structure.
